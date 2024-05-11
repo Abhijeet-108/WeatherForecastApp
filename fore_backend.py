05527b16e66e21f0cdc4b4d52641f281391da6dd
Asus <abhijeet.jmt108@gmail.com>
2024-05-11 10:18:42 +0530

    completed project

--- fore_backend.py
+++ fore_backend.py
@@ -3,19 +3,15 @@ import requests
 API_KEY = "a8e0dfe699a5933f183ed0f4ba5a335b"
 
 
-def get_data(place, forecast_days=None, kind=None):
+def get_data(place, forecast_days=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if kind == "sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 
 if __name__ == "__main__":
-    print(get_data(place="Jamtara",forecast_days=4,kind="Temperature"))
+    print(get_data(place="Jamtara",forecast_days=4))
--- main.py
+++ main.py
@@ -8,8 +8,23 @@ days = st.slider("Forecast Days", min_value=1 , max_value=5 , help="Select the n
 option = st.selectbox("Select data to view", ("Temperature" , "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
+if place:
 
-d, t = get_data(place, days, option)
+    try:
+        filtered_data = get_data(place, days)
 
-figure = px.line(x=d, y=t, labels={"x":"Date", "y":"Temperature(C)",})
-st.plotly_chart(figure)
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] / 10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={"x":"Date", "y":"Temperature(C)",})
+            st.plotly_chart(figure)
+
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png","Snow": "images/snow.png"}
+            sky = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky]
+            print(sky)
+            st.image(image_paths,width=118)
+    except KeyError:
+        st.write("That Place does not Exit...... \n Write Correct Place.")
